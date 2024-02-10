4523dd51dd5d2bb8c3586e5424b0e618cbdeef8c
Shraavan Acharya <shraavan.acharya@gmail.com>
2024-02-10 16:12:53 +0530

    Added functionality to handle multiple instances of ColorPalette assets. Also updated version to 0.0.34

--- Packages/com.shraa1.sdk/Scripts/Editor/Helper/Colourful Hierarchy Category GameObject/Editor/StyleHierarchy.cs
+++ Packages/com.shraa1.sdk/Scripts/Editor/Helper/Colourful Hierarchy Category GameObject/Editor/StyleHierarchy.cs
@@ -1,22 +1,25 @@
+using System.Collections.Generic;
 using UnityEditor;
-
 using UnityEngine;
 
 namespace MStudio {
 	[InitializeOnLoad]
 	public class StyleHierarchy {
 		static string[] dataArray;//Find ColorPalette GUID
-		static string path;//Get ColorPalette(ScriptableObject) path
-		static ColorPalette colorPalette;
+		static List<string> paths = new List<string>();//Get ColorPalette(ScriptableObject) path
+		static List<ColorPalette> colorPalettes = new List<ColorPalette>();
 
 		static StyleHierarchy () {
 			dataArray = AssetDatabase.FindAssets("t:ColorPalette");
 
 			if (dataArray.Length >= 1) {
-				//We have only one color palette, so we use dataArray[0] to get the path of the file
-				path = AssetDatabase.GUIDToAssetPath(dataArray[0]);
-
-				colorPalette = AssetDatabase.LoadAssetAtPath<ColorPalette>(path);
+				paths.Clear();
+				colorPalettes.Clear();
+				for (var i = 0; i < dataArray.Length; i++) {
+					//We have only one color palette, so we use dataArray[0] to get the path of the file
+					paths.Add(AssetDatabase.GUIDToAssetPath(dataArray[i]));
+					colorPalettes.Add(AssetDatabase.LoadAssetAtPath<ColorPalette>(paths[i]));
+				}
 
 				EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindow;
 			}
@@ -29,28 +32,30 @@ namespace MStudio {
 			Object instance = EditorUtility.InstanceIDToObject(instanceID);
 
 			if (instance != null) {
-				for (int i = 0; i < colorPalette.colorDesigns.Count; i++) {
-					var design = colorPalette.colorDesigns[i];
+				for (var i = 0; i < colorPalettes.Count; i++) {
+					for (int j = 0; j < colorPalettes[i].colorDesigns.Count; j++) {
+						var design = colorPalettes[i].colorDesigns[j];
 
-					//Check if the name of each gameObject is begin with keyChar in colorDesigns list.
-					if (instance.name.StartsWith(design.keyChar)) {
-						//Remove the symbol(keyChar) from the name.
-						string newName = instance.name.Substring(design.keyChar.Length);
-						//Draw a rectangle as a background, and set the color.
-						EditorGUI.DrawRect(selectionRect, design.backgroundColor);
+						//Check if the name of each gameObject is begin with keyChar in colorDesigns list.
+						if (instance.name.StartsWith(design.keyChar)) {
+							//Remove the symbol(keyChar) from the name.
+							string newName = instance.name.Substring(design.keyChar.Length);
+							//Draw a rectangle as a background, and set the color.
+							EditorGUI.DrawRect(selectionRect, design.backgroundColor);
 
-						//Create a new GUIStyle to match the desing in colorDesigns list.
-						GUIStyle newStyle = new GUIStyle {
-							alignment = design.textAlignment,
-							fontStyle = design.fontStyle,
-							normal = new GUIStyleState() {
-								textColor = design.textColor,
-							}
-						};
+							//Create a new GUIStyle to match the desing in colorDesigns list.
+							GUIStyle newStyle = new GUIStyle {
+								alignment = design.textAlignment,
+								fontStyle = design.fontStyle,
+								normal = new GUIStyleState() {
+									textColor = design.textColor,
+								}
+							};
 
-						//Draw a label to show the name in upper letters and newStyle.
-						//If you don't like all capital latter, you can remove ".ToUpper()".
-						EditorGUI.LabelField(selectionRect, design.allUppercase ? newName.ToUpper() : newName, newStyle);
+							//Draw a label to show the name in upper letters and newStyle.
+							//If you don't like all capital latter, you can remove ".ToUpper()".
+							EditorGUI.LabelField(selectionRect, design.allUppercase ? newName.ToUpper() : newName, newStyle);
+						}
 					}
 				}
 			}
