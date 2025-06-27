df34da46061f14a3e59cdb7055c08e0bc2dc1c63
Shraavan Acharya <shraavan.acharya@gmail.com>
2025-06-27 13:31:29 +0530

    0.0.54
    Added a few more attributes for Odin Inspector helper.

--- Packages/com.shraa1.sdk/Scripts/Helpers/OdinInspectorHelper.cs
+++ Packages/com.shraa1.sdk/Scripts/Helpers/OdinInspectorHelper.cs
@@ -1,11 +1,26 @@
 using System;
 using System.Diagnostics;
 
-namespace BaseSDK.SirenixHelper
-{
+namespace BaseSDK.SirenixHelper {
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true), Conditional("UNITY_EDITOR")]
-    public class FoldoutGroupAttribute : Attribute
-    {
+    public class FoldoutGroupAttribute : Attribute {
         public FoldoutGroupAttribute(string groupName, bool expanded = false, float order = 0f) { }
     }
+
+    [AttributeUsage(AttributeTargets.All), Conditional("UNITY_EDITOR")]
+    public class DrawWithUnityAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true), Conditional("UNITY_EDITOR")]
+    public class PreviewFieldAttribute : Attribute {
+        public int Height;
+    }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true), Conditional("UNITY_EDITOR")]
+    public class HorizontalGroupAttribute : Attribute {
+        public HorizontalGroupAttribute(float width = 0f, int marginLeft = 0, int marginRight = 0, float order = 0f) { }
+        public HorizontalGroupAttribute(string group, float width = 0f, int marginLeft = 0, int marginRight = 0, float order = 0f) { }
+    }
+
+    [AttributeUsage(AttributeTargets.All), Conditional("UNITY_EDITOR")]
+    public class HideLabelAttribute : Attribute { }
 }
