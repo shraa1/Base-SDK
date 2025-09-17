66cdfafeb8ab013c1a72c209732afa0914b45eb1
Shraavan Acharya <shraavan.acharya@gmail.com>
2025-09-17 21:52:36 +0530

    v0.2.9
    Added HasKey in PlayerPrefsManager
    Json (de)serialize in PlayerPrefsManager now uses the settings object for safe uses of types with self referencing loop, etc.

--- Packages/com.fmfgames.sdk/Scripts/Helpers/PlayerPrefsManager.cs
+++ Packages/com.fmfgames.sdk/Scripts/Helpers/PlayerPrefsManager.cs
@@ -19,16 +19,18 @@ namespace BaseSDK.Helper {
 			if (str.IsNullOrEmpty())
 				return defaultValue;
 
-			var obj = str.Deserialize<PlayerPrefsSaveState>();
-			var deserialized = obj.Encrypted ? obj.Data.Decrypt<string>().Deserialize<T>() : obj.Data.Deserialize<T>();
+			var obj = str.Deserialize<PlayerPrefsSaveState>(GameConstants.JsonSerializerSettings);
+			var deserialized = obj.Encrypted ? obj.Data.Decrypt<string>().Deserialize<T>() : obj.Data.Deserialize<T>(GameConstants.JsonSerializerSettings);
 
 			return useNewtonsoft ? deserialized : JsonUtility.FromJson<T>(str);
 		}
 
 		public static void Set<T> (string key, T value, bool useNewtonsoft = true, bool encrypted = true) {
-			var state = new PlayerPrefsSaveState() { Encrypted = encrypted, Data = encrypted ? value.Serialize().Encrypt() : value.Serialize() };
-			var str = useNewtonsoft ? JsonConvert.SerializeObject(state) : JsonUtility.ToJson(value);
+			var state = new PlayerPrefsSaveState() { Encrypted = encrypted, Data = encrypted ? value.Serialize(GameConstants.JsonSerializerSettings).Encrypt() : value.Serialize(GameConstants.JsonSerializerSettings) };
+			var str = useNewtonsoft ? state.Serialize(GameConstants.JsonSerializerSettings) : JsonUtility.ToJson(value);
 			PlayerPrefs.SetString(key, str);
 		}
+
+		public static bool HasKey(string key) => PlayerPrefs.HasKey(key);
 	}
 }
